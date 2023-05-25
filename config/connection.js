0926c94c6b8c5b01c295cd82f34879da4d9b1733
Aiwin Thomas <aiwin.as789@gmail.com>
2023-05-25 20:19:23 +0530

    hosting first commit

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 module.exports.connect=function(done){
     const url='mongodb://0.0.0.0:27017'
-    const dbname='Shopping'
+    const dbname='Shop'
     mongoClient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
