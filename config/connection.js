282c92ea4d2be8c41f0d07cfaa7e342d65f03955
Aiwin Thomas <aiwin.as789@gmail.com>
2023-05-25 20:56:36 +0530

    Hosting third commit

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 module.exports.connect=function(done){
     const url='mongodb://0.0.0.0:27017'
-    const dbname='Shop'
+    const dbname='Shopping'
     mongoClient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
